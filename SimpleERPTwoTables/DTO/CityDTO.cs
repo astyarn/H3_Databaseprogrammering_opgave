d993c14e57a3a3a6d6e7eda3124f356f7d623b16
Bjoern <bjoe6880@elev.techcollege.dk>
2023-10-24 10:08:50 +0200

    Pasted in Lars' DTO code

---
+++ SimpleERPTwoTables/DTO/CityDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace SimpleERPTwoTables.DTO
+{
+    public class CityForSaveDTO
+    {
+        public string CityName { get; set; }
+
+        public string CityDescription { get; set; }
+
+        public int CountryId { get; set; }
+    }
+
+    public class CityForUpdateDTO : CityForSaveDTO
+    {
+        public int CityId { get; set; }
+    }
+
+    public class CityDTO : CityForUpdateDTO
+    {
+        public CountryDTOMinusRelations? Country { get; set; }
+    }
+
+    public class CityDTOMinusRelations : CityForUpdateDTO
+    {
+
+    }
+}
---
+++ SimpleERPTwoTables/DTO/CountryDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleERPTwoTables.DTO
+{
+    public class CountryForSaveDTO
+    {
+        public string CountryName { get; set; }
+    }
+
+    public class CountryForUpdateDTO : CountryForSaveDTO
+    {
+        public int CountryId { get; set; }
+    }
+
+    public class CountryDTO : CountryForUpdateDTO
+    {
+        public ICollection<CityDTOMinusRelations> Cities { get; set; }
+    }
+
+    public class CountryDTOMinusRelations : CountryForUpdateDTO
+    {
+
+    }
+}
