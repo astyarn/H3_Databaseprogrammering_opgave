4bb37993b04741117a2326471adea46a195c3f0e
Bjoern <bjoe6880@elev.techcollege.dk>
2023-10-25 12:19:27 +0200

    Added new DTO's

---
+++ SimpleERPTwoTables/DTO/CityLanguageDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleERPTwoTables.DTO
+{
+    public class CityLanguageForSaveAndUpdateDTO
+    {
+        public int CityId { get; set; }
+
+        public int LanguageId { get; set; }
+    }
+
+    public class CityLanguageDTO : CityLanguageForSaveAndUpdateDTO
+    {
+        public CityDTOMinusRelations City { get; set; }
+
+        public LanguageDTOMinusRelations Language { get; set; }
+    }
+
+    public class CityLanguageMinusCityDTO : CityLanguageForSaveAndUpdateDTO
+    {
+        public LanguageDTOMinusRelations Language { get; set; }
+    }
+
+
+}
---
+++ SimpleERPTwoTables/DTO/LanguageDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleERPTwoTables.DTO
+{
+    public class LanguageForSaveDTO
+    {
+        public string LanguageName { get; set; }
+    }
+
+    public class LanguageForUpdateDTO : LanguageForSaveDTO
+    {
+        public int LanguageId { get; set; }
+    }
+
+    public class LanguageDTO : LanguageForUpdateDTO
+    {
+        public CityLanguageDTO CityLanguage { get; set; }
+    }
+
+    public class LanguageDTOMinusRelations : LanguageForUpdateDTO
+    {
+
+    }
+}
